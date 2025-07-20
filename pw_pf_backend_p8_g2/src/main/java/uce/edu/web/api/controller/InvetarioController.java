1946e245162157cdef9e75e056fb1a5430a06112
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-07-20 13:19:10 -0500

    Creación de controller de Inventario con CRUD

---
+++ pw_pf_backend_p8_g2/src/main/java/uce/edu/web/api/controller/InvetarioController.java
@@ -0,0 +1,63 @@
+package uce.edu.web.api.controller;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import uce.edu.web.api.service.IInventarioService;
+
+@Path("/inventarios")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class InvetarioController {
+
+    @Inject
+    private IInventarioService iInventarioService;
+
+    @GET
+    @Path("/{codigoBodega}")
+    @Operation(summary = "Obtener el stock de un producto en la bodega", description = "Esta capacidad me permite obtener el stock de un producto registrada en una bodega")
+    public Response consultarStockProductoEnBodega(@PathParam("codigoBodega") String codigoBodega,
+            @QueryParam("codigoBarras") String codigoBarras) {
+        Integer stockActual = this.iInventarioService.buscarStock(codigoBodega, codigoBarras);
+        return Response.status(Response.Status.OK).entity(stockActual).build();
+    }
+
+    @POST
+    @Path("/{codigoBodega}/{codigoBarras}")
+    @Operation(summary = "Guardar un producto en Bodega", description = "Esta capacidad me permite guardar un producto en bodega con un stock")
+    public Response insertarStockProductoEnBodega(@PathParam("codigoBodega") String codigoBodega,
+            @PathParam("codigoBarras") String codigoBarras, @QueryParam("stock") Integer stock) {
+        this.iInventarioService.guardar(codigoBodega, codigoBarras, stock);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/{codigoBodega}/{codigoBarras}")
+    @Operation(summary = "Actulizar el stock de un Producto en Bodega", description = "Esta capacidad me permite actulizar el sotck de un producto en una Bodega")
+    public Response actualizarStockProductoEnBodega(@PathParam("codigoBodega") String codigoBodega,
+            @PathParam("codigoBarras") String codigoBarras, @QueryParam("nuevoStock") Integer nuevoStock) {
+        this.iInventarioService.actualizarStock(codigoBodega, codigoBarras, nuevoStock);
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    @DELETE
+    @Path("/{codigoBodega}")
+    @Operation(summary = "Eliminar un Producto de una Bodega", description = "Elimina la relación de un producto específico con una bodega específica")
+    public Response eliminarProductoDeBodega(@PathParam("codigoBodega") String codigoBodega,
+            @QueryParam("codigoBarras") String codigoBarras) {
+        this.iInventarioService.borrar(codigoBodega, codigoBarras);
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+}
