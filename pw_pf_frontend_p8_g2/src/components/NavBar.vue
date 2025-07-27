585e2990071a6c07a25ca998f03dc8be22b691ea
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-07-27 10:22:05 -0500

    Creación de nabvar interactivo

--- pw_pf_frontend_p8_g2/src/components/NavBar.vue
+++ pw_pf_frontend_p8_g2/src/components/NavBar.vue
@@ -5,19 +5,22 @@
     <router-link class="link" to="/bodega">Bodega</router-link>
     <a class="link" @click="isMercancia = !isMercancia">Mercancia</a>
     <div v-if="isMercancia" class="container_mercancia">
-      <router-link class="link prod" to="/producto"
-        >Produto</router-link
-      >
-      <router-link class="link prod" to="/servicio"
-        >Servicio</router-link
-      >
+      <router-link class="link prod" to="/producto">Produto</router-link>
+      <router-link class="link prod" to="/servicio">Servicio</router-link>
     </div>
     <router-link class="link" to="/cliente">Cliente</router-link>
     <router-link class="link" to="/factura">Factura</router-link>
     <router-link class="link" to="/reporte">Reporte</router-link>
   </nav>
   <div :class="['mostrar', !ocultar ? 'container-esconder' : 'container-mostrar']">
-    <button @click="esconder()">{{ flecha }}</button>
+    <button @click="esconder()">
+      <span v-if="ocultar" class="icon-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      <span v-else class="icon-x">✖</span>
+    </button>
   </div>
 </template>
 
@@ -26,18 +29,12 @@ export default {
   data() {
     return {
       ocultar: false,
-      flecha: "<",
-      isMercancia: true,
+      isMercancia: false,
     };
   },
   methods: {
     esconder() {
       this.ocultar = !this.ocultar;
-      if (this.ocultar) {
-        this.flecha = ">";
-      } else {
-        this.flecha = "<";
-      }
     },
     mostrarMecancia() {
       this.isMercancia = !this.isMercancia;
@@ -51,11 +48,11 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: start;
-  background-color: white;
+  background-color: #07265c;
   border-radius: 0px 20px 20px 0px;
   box-shadow: 10px 0px 10px rgba(0, 0, 0, 0.407);
   gap: 1px;
-  padding: 0 30px;
+  padding: 0 10px;
   padding-top: 50px;
   width: 250px;
   height: 100vh;
@@ -79,15 +76,26 @@ export default {
 }
 
 .link {
-  border: 1px solid;
   padding: 10px 20px;
   font-size: 15px;
   transition: all 0.5s ease-in;
   text-decoration: none;
-  color: black;
+  color: #ffffff;
   cursor: pointer;
   box-sizing: border-box;
   text-align: left;
+  transition: background-color 0.1s ease, color 0.1s ease,
+    border-radius 0.1s ease;
+}
+
+.link:hover {
+  background-color: #11387a;
+  border-radius: 5px;
+}
+
+.link:active {
+  background-color: #0d2f6a;
+  color: #ffffff;
 }
 
 .container_mercancia {
@@ -100,14 +108,14 @@ export default {
   margin-left: 15px;
 }
 
-.mostrar{
+.mostrar {
   display: flex;
   justify-content: center;
   align-items: center;
   position: fixed;
   top: 5px;
-  left: 190px; 
-  transition: all 0.4s ease-in-out; 
+  left: 190px;
+  transition: all 0.4s ease-in-out;
 }
 
 button {
@@ -116,7 +124,35 @@ button {
   justify-content: center;
   align-items: center;
   cursor: pointer;
-  border-radius: 100%;
+  border-radius: 5px;
+  background: none;
+  border: none;
+}
+
+/* Icono hamburguesa */
+.icon-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+}
+
+.icon-menu span {
+  display: block;
+  width: 22px;
+  height: 3px;
+  background: black;
+  margin: 2px 0;
+  border-radius: 2px;
+}
+
+/* Icono X */
+.icon-x {
+  font-size: 22px;
+  color: #fff;
+  line-height: 1;
 }
 
 .container-esconder {
@@ -124,6 +160,6 @@ button {
 }
 
 .container-mostrar {
-  left: 10px;
+  left: 0px;
 }
 </style>
